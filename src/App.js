3d42a9c9165d8c25e3dbc8108a9798f9ce3bab2b
Han <59479453+recta-rrae@users.noreply.github.com>
2022-03-22 17:36:11 +0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'assets/css/style.css';
+
+function App() {
+  return (
+    <div className="mx-auto mt-20 bg-white shadow-md rounded-md p-5">
+      <h1 className="text-3xl font-bold flex justify-center">React Memberpage</h1>
+    </div>
+  );
+}
+
+export default App;
